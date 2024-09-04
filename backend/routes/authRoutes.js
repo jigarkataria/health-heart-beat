dc7a62771c47049e39a3acdbd8102305742817a5
Jigar Kataria <jigarkataria@Jigars-MacBook-Pro.local>
2024-09-04 22:52:06 +0530

    update signup endpoint

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -48,8 +48,8 @@ router.post('/signup', async (req, res, next) => {
 router.post('/updateUser', async (req, res, next) => {
   try {
     const { password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
     if(password) {
+      const hashedPassword = await bcrypt.hash(password, 10);
       req.body.password = hashedPassword;
     }
     const user = await User.findOneAndUpdate({email : req.body.email}, req.body)
